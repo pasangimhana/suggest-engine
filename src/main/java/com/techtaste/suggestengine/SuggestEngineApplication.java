26dc2c06aabc37e33ac22ea661c56b89f13c05ea
Pasan Gimhana <pasangimhanaofficial@gmail.com>
2024-06-04 02:32:23 +0530

    Initial commit

---
+++ src/main/java/com/techtaste/suggestengine/SuggestEngineApplication.java
@@ -0,0 +1,13 @@
+package com.techtaste.suggestengine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SuggestEngineApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SuggestEngineApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/techtaste/suggestengine/SuggestEngineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.techtaste.suggestengine;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SuggestEngineApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
